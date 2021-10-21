f2cc01679f7aad0d85d92a7de4fd3adeb327d911
komin92 <komin92ccna@gmail.com>
2021-10-21 09:40:52 +0630

    Update config.py

--- config/config.py
+++ config/config.py
@@ -7,20 +7,6 @@ from time import strftime, localtime
 
 t = localtime()
 time_string = strftime("%m-%d-%Y", t)
-# --- data ---
-# profiles = ['kbz-security-sso', 'sap-prod-sso', 'kbz-crayon-sso', 'kbz-temenos-i-sso', 'kbz-kbzt-sso']
-profiles = ['kbz-kbzt-sso']
-
-# --- file write formart ---
-file_name = "route_id"
-sfile_name = "new_route_tableIds"
-efile_name = "route_tables_info"
-output = "json"
-fformat = "txt"
-
-##########################
-recordRouteId = []
-
 
 class LOG:
     log_filepath = "../log/"
@@ -70,18 +56,3 @@ def file_read_handler(file_name, rformat):
                 test = json.load(f)
         return test
 
-
-# Simple app don't used
-def writeTempFile(text=None):
-    filePath = "/temp/file1.txt"
-    if not text:  # If not provided return file content
-        f = open(filePath, "r")
-        slug = f.read()
-        return slug
-    else:
-        f = open(filePath, "a")  # Create a blank file
-        f.seek(0)  # sets  point at the beginning of the file
-        f.truncate()  # Clear previous content
-        f.write(text)  # Write file
-        f.close()  # Close file
-        return text
